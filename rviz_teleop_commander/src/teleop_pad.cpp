8b72778bce6e9be84615eb1555decf6619b62384
shantengfei <944968456@qq.com>
2017-12-07 16:49:53 +0800

    面板改造可以了，但是存在的问题是不能实时反馈信息，在考虑是不是单线程造成的问题，这个版本是在update函数中验证不是label的问题，确实不是label的问题，现在要尝试解决这个问题

--- rviz_teleop_commander/src/teleop_pad.cpp
+++ rviz_teleop_commander/src/teleop_pad.cpp
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 #include <QPainter>
 #include <QLineEdit>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
-#include <QLabel>
 #include <QTimer>
 
 #include <geometry_msgs/Twist.h>
@@ -16,27 +14,41 @@ namespace rviz_teleop_commander
 {
 
 // 构造函数，初始化变量
-TeleopPanel::TeleopPanel( QWidget* parent )
-  : rviz::Panel( parent )
-  , linear_velocity_( 0 )
-  , angular_velocity_( 0 )
+TeleopPanel::TeleopPanel( QWidget* parent ) : rviz::Panel( parent )
 {
-
-  // 创建一个输入topic命名的窗口
-  QVBoxLayout* topic_layout = new QVBoxLayout;
-  topic_layout->addWidget( new QLabel( "Teleop Topic:" ));
-  output_topic_editor_ = new QLineEdit;
-  topic_layout->addWidget( output_topic_editor_ );
-
+  
+      // 创建一个输入topic命名的窗口
+      QVBoxLayout *topic_layout = new QVBoxLayout;
+  topic_layout->addWidget( new QLabel( "joint1" ));
+  // output_topic_editor_ = new QLineEdit;
+  // topic_layout->addWidget(output_topic_editor_);
+   QLjoint1 = new QLabel;
+   topic_layout->addWidget(QLjoint1);
   // 创建一个输入线速度的窗口
-  topic_layout->addWidget( new QLabel( "Linear Velocity:" ));
-  output_topic_editor_1 = new QLineEdit;
-  topic_layout->addWidget( output_topic_editor_1 );
+  topic_layout->addWidget( new QLabel( "joint2" ));
+//  output_topic_editor_1 = new QLineEdit;
+//  topic_layout->addWidget( output_topic_editor_1 );
+  QLjoint2 = new QLabel;
+  topic_layout->addWidget(QLjoint2);
 
   // 创建一个输入角速度的窗口
-  topic_layout->addWidget( new QLabel( "Angular Velocity:" ));
-  output_topic_editor_2 = new QLineEdit;
-  topic_layout->addWidget( output_topic_editor_2 );
+  topic_layout->addWidget( new QLabel( "joint3" ));
+//  output_topic_editor_2 = new QLineEdit;
+//  topic_layout->addWidget( output_topic_editor_2 );
+  QLjoint3 = new QLabel;
+  topic_layout->addWidget(QLjoint3);
+
+  topic_layout->addWidget(new QLabel("joint4"));
+  QLjoint4 = new QLabel;
+  topic_layout->addWidget(QLjoint4);
+
+  topic_layout->addWidget(new QLabel("joint5"));
+  QLjoint5 = new QLabel;
+  topic_layout->addWidget(QLjoint5);
+
+  topic_layout->addWidget(new QLabel("joint6"));
+  QLjoint6 = new QLabel;
+  topic_layout->addWidget(QLjoint6);
 
   QHBoxLayout* layout = new QHBoxLayout;
   layout->addLayout( topic_layout );
@@ -46,102 +58,139 @@ TeleopPanel::TeleopPanel( QWidget* parent )
   QTimer* output_timer = new QTimer( this );
 
   // 设置信号与槽的连接
-  connect( output_topic_editor_, SIGNAL( editingFinished() ), this, SLOT( updateTopic() ));             // 输入topic命名，回车后，调用updateTopic()
-  connect( output_topic_editor_1, SIGNAL( editingFinished() ), this, SLOT( update_Linear_Velocity() )); // 输入线速度值，回车后，调用update_Linear_Velocity()
-  connect( output_topic_editor_2, SIGNAL( editingFinished() ), this, SLOT( update_Angular_Velocity() ));// 输入角速度值，回车后，调用update_Angular_Velocity()
+  // connect( output_topic_editor_, SIGNAL( editingFinished() ), this, SLOT( updateTopic() ));             // 输入topic命名，回车后，调用updateTopic()
+  // connect( output_topic_editor_1, SIGNAL( editingFinished() ), this, SLOT( update_Linear_Velocity() )); // 输入线速度值，回车后，调用update_Linear_Velocity()
+  // connect( output_topic_editor_2, SIGNAL( editingFinished() ), this, SLOT( update_Angular_Velocity() ));// 输入角速度值，回车后，调用update_Angular_Velocity()
 
   // 设置定时器的回调函数，按周期调用sendVel()
-  connect( output_timer, SIGNAL( timeout() ), this, SLOT( sendVel() ));
-
+  //connect( output_timer, SIGNAL( timeout() ), this, SLOT( sendVel() ));
+  connect(output_timer, SIGNAL(timeout()), this, SLOT(update_joint_value()));
   // 设置定时器的周期，100ms
-  output_timer->start( 100 );
+  output_timer->start( 10 );
+
+} //构造函数的结尾
+
+void TeleopPanel::chatterCB(const sensor_msgs::JointState &msg)
+{
+ for(int i=0;i<6;i++)
+ joint[i]=msg.position[i];
+  QLjoint1->setNum(joint[0]);
+  QLjoint2->setNum(joint[1]);
+  QLjoint3->setNum(joint[2]);
+  QLjoint4->setNum(joint[3]);
+  QLjoint5->setNum(joint[4]);
+  QLjoint6->setNum(joint[5]);
+}
+
+    void TeleopPanel::update_joint_value() 
+{
+  int i, k;
+  static double testNum = 0.1;
+
+  for(i=0;i<6;i++)
+  {
+
+  testNum+=0.1;
+  joint[i]=testNum;
+    if(i==0)
+    ROS_INFO("%f",joint[0]);
+  }
+  QLjoint1->setNum(joint[0]);
+  QLjoint2->setNum(joint[1]);
+  QLjoint3->setNum(joint[2]);
+  QLjoint4->setNum(joint[3]);
+  QLjoint5->setNum(joint[4]);
+  QLjoint6->setNum(joint[5]);
+    // if (ros::ok())
+    //   jointSub =
+    //       nh_.subscribe("joint_states", 1000, &TeleopPanel::chatterCB, this);
 }
 
 // 更新线速度值
-void TeleopPanel::update_Linear_Velocity()
-{
-    // 获取输入框内的数据
-    QString temp_string = output_topic_editor_1->text();
+// void TeleopPanel::update_Linear_Velocity()
+// {
+//     // 获取输入框内的数据
+//     QString temp_string = output_topic_editor_1->text();
 	
-	// 将字符串转换成浮点数
-    float lin = temp_string.toFloat();  
+// 	// 将字符串转换成浮点数
+//     float lin = temp_string.toFloat();  
 	
-	// 保存当前的输入值
-    linear_velocity_ = lin;
-}
+// 	// 保存当前的输入值
+//     linear_velocity_ = lin;
+// }
 
 // 更新角速度值
-void TeleopPanel::update_Angular_Velocity()
-{
-    QString temp_string = output_topic_editor_2->text();
-    float ang = temp_string.toFloat() ;  
-    angular_velocity_ = ang;
-}
+// void TeleopPanel::update_Angular_Velocity()
+// {
+//     QString temp_string = output_topic_editor_2->text();
+//     float ang = temp_string.toFloat() ;  
+//     angular_velocity_ = ang;
+// }
 
 // 更新topic命名
-void TeleopPanel::updateTopic()
-{
-  setTopic( output_topic_editor_->text() );
-}
+// void TeleopPanel::updateTopic()
+// {
+//   setTopic( output_topic_editor_->text() );
+// }
 
 // 设置topic命名
-void TeleopPanel::setTopic( const QString& new_topic )
-{
-  // 检查topic是否发生改变.
-  if( new_topic != output_topic_ )
-  {
-    output_topic_ = new_topic;
+// void TeleopPanel::setTopic( const QString& new_topic )
+// {
+//   // 检查topic是否发生改变.
+//   if( new_topic != output_topic_ )
+//   {
+//     output_topic_ = new_topic;
 	
-    // 如果命名为空，不发布任何信息
-    if( output_topic_ == "" )
-    {
-      velocity_publisher_.shutdown();
-    }
-	// 否则，初始化publisher
-    else
-    {
-      velocity_publisher_ = nh_.advertise<geometry_msgs::Twist>( output_topic_.toStdString(), 1 );
-    }
+//     // 如果命名为空，不发布任何信息
+//     if( output_topic_ == "" )
+//     {
+//       velocity_publisher_.shutdown();
+//     }
+// 	// 否则，初始化publisher
+//     else
+//     {
+//       velocity_publisher_ = nh_.advertise<geometry_msgs::Twist>( output_topic_.toStdString(), 1 );
+//     }
 
-    Q_EMIT configChanged();
-  }
-}
+//     Q_EMIT configChanged();
+//   }
+// }
 
 // 发布消息
-void TeleopPanel::sendVel()
-{
-  if( ros::ok() && velocity_publisher_ )
-  {
-    geometry_msgs::Twist msg;
-    msg.linear.x = linear_velocity_;
-    msg.linear.y = 0;
-    msg.linear.z = 0;
-    msg.angular.x = 0;
-    msg.angular.y = 0;
-    msg.angular.z = angular_velocity_;
-    velocity_publisher_.publish( msg );
-  }
-}
+// void TeleopPanel::sendVel()
+// {
+//   if( ros::ok() && velocity_publisher_ )
+//   {
+//     geometry_msgs::Twist msg;
+//     msg.linear.x = linear_velocity_;
+//     msg.linear.y = 0;
+//     msg.linear.z = 0;
+//     msg.angular.x = 0;
+//     msg.angular.y = 0;
+//     msg.angular.z = angular_velocity_;
+//     velocity_publisher_.publish( msg );
+//   }
+// }
 
 // 重载父类的功能
-void TeleopPanel::save( rviz::Config config ) const
-{
-  rviz::Panel::save( config );
-  config.mapSetValue( "Topic", output_topic_ );
-}
+// void TeleopPanel::save( rviz::Config config ) const
+// {
+//   rviz::Panel::save( config );
+//   config.mapSetValue( "Topic", output_topic_ );
+// }
 
-// 重载父类的功能，加载配置数据
-void TeleopPanel::load( const rviz::Config& config )
-{
-  rviz::Panel::load( config );
-  QString topic;
-  if( config.mapGetString( "Topic", &topic ))
-  {
-    output_topic_editor_->setText( topic );
-    updateTopic();
-  }
-}
-
+// // 重载父类的功能，加载配置数据
+// void TeleopPanel::load( const rviz::Config& config )
+// {
+//   rviz::Panel::load( config );
+//   QString topic;
+//   if( config.mapGetString( "Topic", &topic ))
+//   {
+//     output_topic_editor_->setText( topic );
+//     updateTopic();
+//   }
+// }
+  
 } // end namespace rviz_teleop_commander
 
 // 声明此类是一个rviz的插件
--- rviz_teleop_commander/src/teleop_pad.h
+++ rviz_teleop_commander/src/teleop_pad.h
@@ -5,7 +5,8 @@
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <rviz/panel.h>   //plugin基类的头文件
-
+#include <QLabel>
+#include <sensor_msgs/JointState.h>
 class QLineEdit;
 
 namespace rviz_teleop_commander
@@ -18,47 +19,58 @@ Q_OBJECT
 public:
   // 构造函数，在类中会用到QWidget的实例来实现GUI界面，这里先初始化为0即可
   TeleopPanel( QWidget* parent = 0 );
+  static double testNum;
+      // 重载rviz::Panel积累中的函数，用于保存、加载配置文件中的数据，在我们这个plugin
+      // 中，数据就是topic的名称
+      // virtual void load( const rviz::Config& config );
+      // virtual void save( rviz::Config config ) const;
 
-  // 重载rviz::Panel积累中的函数，用于保存、加载配置文件中的数据，在我们这个plugin
-  // 中，数据就是topic的名称
-  virtual void load( const rviz::Config& config );
-  virtual void save( rviz::Config config ) const;
-
-  // 公共槽.
-public Q_SLOTS:
-  // 当用户输入topic的命名并按下回车后，回调用此槽来创建一个相应名称的topic publisher
-  void setTopic( const QString& topic );
+      // 公共槽.
+      public Q_SLOTS :
+      // 当用户输入topic的命名并按下回车后，回调用此槽来创建一个相应名称的topic
+      // publisher
+      void
+      setTopic(const QString &topic);
 
   // 内部槽.
 protected Q_SLOTS:
-  void sendVel();                 // 发布当前的速度值
-  void update_Linear_Velocity();  // 根据用户的输入更新线速度值
-  void update_Angular_Velocity(); // 根据用户的输入更新角速度值
-  void updateTopic();             // 根据用户的输入更新topic name
-
-  // 内部变量.
+  // void sendVel();                 // 发布当前的速度值
+  // void update_Linear_Velocity();  // 根据用户的输入更新线速度值
+  // void update_Angular_Velocity(); // 根据用户的输入更新角速度值
+  // void updateTopic();             // 根据用户的输入更新topic name
+  void update_joint_value();               //更新各个关节的信息
+      // 内部变量.
 protected:
-  // topic name输入框
-  QLineEdit* output_topic_editor_;
-  QString output_topic_;
-  
+      // topic name输入框
+      //  QLineEdit* output_topic_editor_;
+      //   QString output_topic_;
+      QLabel *QLjoint1;
+  QLabel *QLjoint2;
+  QLabel *QLjoint3;
+  QLabel *QLjoint4;
+  QLabel *QLjoint5;
+  QLabel *QLjoint6;
+  double joint[6]={0};
+
+  void chatterCB(const sensor_msgs::JointState &msg);
   // 线速度值输入框
-  QLineEdit* output_topic_editor_1;
-  QString output_topic_1;
-  
+  // QLineEdit* output_topic_editor_1;
+  // QString output_topic_1;
+
   // 角速度值输入框
-  QLineEdit* output_topic_editor_2;
-  QString output_topic_2;
-  
+//  QLineEdit* output_topic_editor_2;
+//   QString output_topic_2;
+ 
+
   // ROS的publisher，用来发布速度topic
   ros::Publisher velocity_publisher_;
-
+  ros::Subscriber jointSub;
   // The ROS node handle.
   ros::NodeHandle nh_;
 
   // 当前保存的线速度和角速度值
-  float linear_velocity_;
-  float angular_velocity_;
+  // float linear_velocity_;
+  // float angular_velocity_;
 };
 
 } // end namespace rviz_teleop_commander
